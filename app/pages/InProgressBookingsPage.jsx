b6ddf31d9a6e47f874d57ef6e16d784c1afbd2b5
Kunal Rana <121399968+Kunalrana1226@users.noreply.github.com>
2023-12-07 23:02:34 +0530

    Update InProgressBookingsPage.jsx

--- app/pages/InProgressBookingsPage.jsx
+++ app/pages/InProgressBookingsPage.jsx
@@ -1,14 +1,97 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const InProgressBookingsPage = () => {
+const InProgressBookings = () => {
+  const [inProgressList, setInProgressList] = useState([
+    {
+      name: "Govinda",
+      service: "Electrical Repair",
+      arrivalTime: "Jun 6, 2023",
+      //image: require("./images/progress.png"),
+    },
+    {
+      name: "Govinda",
+      service: "Electrical Repair",
+      arrivalTime: "Jun 6, 2023",
+      //image: require("./images/progress.png"),
+    },
+    {
+      name: "Govinda",
+      service: "Electrical Repair",
+      arrivalTime: "Jun 6, 2023",
+      //image: require("./images/progress.png"),
+    },
+  ]);
+
   return (
-    <View>
-      <Text>InProgressBookingsPage</Text>
+    <View style={styles.container}>
+      {inProgressList.map((item, index) => (
+        <View style={styles.box} key={index}>
+          <View style={styles.rectangle}>
+            <View style={styles.card}>
+              <View style={styles.infoContainer}>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text style={styles.service}>{item.service}</Text>
+                <Text style={styles.arrivalTime}>{item.arrivalTime}</Text>
+              </View>
+              <View style={styles.imageContainer}>
+                {/* Replace the image source with your actual image */}
+                <Image source={item.image} style={styles.image} />
+              </View>
+            </View>
+          </View>
+        </View>
+      ))}
     </View>
-  )
-}
+  );
+};
 
-export default InProgressBookingsPage
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  box: {
+    height: 90,
+    width: 300,
+    marginBottom: 8,
+  },
+  rectangle: {
+    backgroundColor: '#c0b9dd',
+    borderRadius: 20,
+    height: 90,
+    width: 300,
+    padding: 16,
+  },
+  card: {
+    backgroundColor: "#c0b9dd",
+    flexDirection: "row",
+  },
+  infoContainer: {
+    flex: 2,
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  service: {
+    fontSize: 14,
+  },
+  arrivalTime: {
+    fontSize: 12,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default InProgressBookings;
